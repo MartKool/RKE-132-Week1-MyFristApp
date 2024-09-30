5cdd60b42a317829472f08b1d25624d66b390576
Mart-Hannes Reinfeldt <mart-hannes.reinfeldt@tktk.ee>
2024-09-30 15:30:06 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// rakendus küsib kasutajalt sisestada nimi
+// rakendus tervitab nime pidi
+
+
+Console.WriteLine("Enter your name");
+// sõne-string
+string userName;
+
+userName = Console.ReadLine();
+Console.WriteLine("Hello"+", "+userName+"!");
+
+// string interpolation
+Console.WriteLine($"Hello {userName}!");
